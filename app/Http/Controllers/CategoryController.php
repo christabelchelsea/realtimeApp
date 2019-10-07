b20c775f93414fe73b5c5a45728c43a7b1ab47b8
christabelchelsea <christabel.chelsea@gmail.com>
2019-10-07 14:02:44 +0700

    CRUD Category

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -14,72 +14,36 @@ class CategoryController extends Controller
      */
     public function index()
     {
-        //
-    }
+        return category::latest()->get();
+    }  
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
-    }
-
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
     public function store(Request $request)
-    {
-        //
+    { 
+        $category = new Category;
+        $category->name = $request->name;
+        $category->slug = str_slug($request->name);
+        $category->save();
+        return response('Created', 201);
     }
-
-    /**
-     * Display the specified resource.
-     *
-     * @param  \App\Model\Category  $category
-     * @return \Illuminate\Http\Response
-     */
+    
     public function show(Category $category)
     {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  \App\Model\Category  $category
-     * @return \Illuminate\Http\Response
-     */
-    public function edit(Category $category)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \App\Model\Category  $category
-     * @return \Illuminate\Http\Response
-     */
+        return $category;
+    } 
+    
     public function update(Request $request, Category $category)
     {
-        //
-    }
+        $category->update([
+            'name' => $request->name,
+            'slug' => str_slug($request->name)
+        ]);
 
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  \App\Model\Category  $category
-     * @return \Illuminate\Http\Response
-     */
+        return response('Updated', 202);
+    }
+    
     public function destroy(Category $category)
     {
-        //
+        $category->delete();
+        return response(null, 204);
     }
 }
