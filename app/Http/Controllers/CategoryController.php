75d4cda300e628a901db8f7a95b6773837f0691b
christabelchelsea <christabel.chelsea@gmail.com>
2019-10-07 23:01:10 +0700

    JWT Middleware and Exception

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -8,11 +8,11 @@ use App\HTTP\Resources\CategoryResource;
 
 class CategoryController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
+    public function __construct()
+    { 
+        $this->middleware('JWT', ['except' => ['index', 'show']]);
+    }
+    
     public function index()
     {
         return CategoryResource::collection(category::latest()->get());
